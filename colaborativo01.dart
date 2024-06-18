0d2fa125f9a378fbe86177751674d866b81ed744
Julian Salazar <jsalazar@sena.edu.co>
2024-06-18 10:45:25 -0500

    Funcionalidad para buscar un elemento en la lista

--- colaborativo01.dart
+++ colaborativo01.dart
@@ -3,9 +3,16 @@ void main() {
 
   List<int> numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   // Mostrar la lista
-  var i;
-  for(i = 0; i < numeros.length; i++){
+  for (int i = 0; i < numeros.length; i++) {
     print("El vector es ");
     print(numeros[i]);
+  }
+
+  // Código para buscar un elemento en la lista
+  int numBuscar = 3;
+  for (int i = 0; i < numeros.length; i++) {
+    if (numBuscar == numeros[i]) {
+      print("Se encontró el número en la posición $i");
+    }
   }
 }
